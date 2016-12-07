615162e979c6b81d5566a68f0b908f6197c0f95e
x404 <kronos2003@gmail.com>
2016-12-07 21:18:08 +0200

    Добавлены шрифты, сделано боковое меню

--- js/engine.js
+++ js/engine.js
@@ -2,39 +2,48 @@ $(document).ready(function(){
 	$('.panel nav li a').hover(
 		function(){
 			var $this = $(this),
+				$parent = $this.parent(),
+				$current = $this.closest('ul').find('.current');
 				menuItem = $this.data('name');
 
 			if (typeof menuItem !== 'undefined'){
-				$('body').addClass('l-with-subnav1');				
+				$('body').addClass('l-with-subnav1');
 			} else {
 				$('body').removeClass('l-with-subnav1');
-			};
+			};			
 
-			
+			$current.removeClass('current');
+			$parent.addClass('current');
 
 			$('.subnav-lev1 .subnav_content-active').removeClass('subnav_content-active');
 			$('.subnav-lev1 .subnav_content-' + menuItem).addClass('subnav_content-active');
 
-	}
-	);
+			// удаляем наведения со 2го уровня
+			$('.subnav-lev1').find('.current').removeClass('current');
+	});
 
 
 	$('.subnav-lev1 .subnav_content ul li a').mouseenter(function(){
 			var $this = $(this),
+				$parent = $this.parent(),
+				$currents = $this.closest('ul').find('.current');
 				menuItem = $this.data('name');
-console.log("!");
-			if (typeof menuItem !== 'undefined'){
-				$('body').addClass('l-with-subnav2');				
-			} else {
-				$('body').removeClass('l-with-subnav2');
-			};
-			
 
-			$('.subnav-lev2 .subnav_content-active').removeClass('subnav_content-active');
-			$('.subnav-lev2 .subnav_content-' + menuItem).addClass('subnav_content-active');
+		if (typeof menuItem !== 'undefined'){
+			$('body').addClass('l-with-subnav2');
+		} else {
+			$('body').removeClass('l-with-subnav2');
+		};
+		
 
-	}
-	);
+		// удаляем наведения со 2го уровня и добавляем новое
+		$currents.removeClass('current');
+		$parent.addClass('current');
+
+
+		$('.subnav-lev2 .subnav_content-active').removeClass('subnav_content-active');
+		$('.subnav-lev2 .subnav_content-' + menuItem).addClass('subnav_content-active');
+	});
 
 
 
